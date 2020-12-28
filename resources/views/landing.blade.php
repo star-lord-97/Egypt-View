fb7c40413757f3864e99c6a7efb79be92a9c9e65
star-lord-97 <a_mahmoud_1997@yahoo.com>
2020-12-28 09:02:28 +0200

    added vue.js 3 scaffolding

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Egypt-View</title>
+
+    {{-- JS --}}
+    <script src="/js/app.js" defer></script>
+</head>
+
+<body>
+    <div id="app"></div>
+</body>
+
+</html>
